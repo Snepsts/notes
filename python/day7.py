853abb64f270c3436455a738f702ffd0b16ff203
Michael Ranciglio <michael.ranciglio@wwinc.com>
2018-04-23 09:05:19 -0500

    Add beginning of day7

---
+++ python/day7.py
@@ -0,0 +1,9 @@
+#!/bin/python3
+import sys
+
+def main():
+    n = int(input().strip())
+    arr = [int(arr_temp) for arr_temp in input().strip().split(' ')]
+
+if __name__ == '__main__':
+    main()
