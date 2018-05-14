e3796315c34365a3ea761cc2a28eb9d85e9f9fc7
Michael Ranciglio <michael.ranciglio@wwinc.com>
2018-05-14 12:58:46 -0500

    Finish day7

--- python/day7.py
+++ python/day7.py
@@ -5,5 +5,17 @@ def main():
     n = int(input().strip())
     arr = [int(arr_temp) for arr_temp in input().strip().split(' ')]
 
+    for i in range(n):
+        print(arr[n-i-1], end=" ")
+
 if __name__ == '__main__':
     main()
+
+''' test_input:
+4
+1 4 3 2
+'''
+
+''' expected_output:
+2 3 4 1
+'''
