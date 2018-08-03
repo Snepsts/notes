bb9a057ceef247111e33ca5b02ef94aeb984d7af
Michael Ranciglio <rancigliom@gmail.com>
2018-08-02 21:06:28 -0500

    Add initial cmake tutorial stuff

---
+++ cmake/hello_world.cpp
@@ -0,0 +1,13 @@
+//extremely complex cpp program
+#include <iostream>
+#include "hello_world_config.h"
+
+int main()
+{
+	std::cout << "first_try version "
+                  << first_try_VERSION_MAJOR << "."
+	          << first_try_VERSION_MINOR << '\n';
+	std::cout << "Hello, World!\n";
+
+	return 0;
+}
---
+++ cmake/hello_world_config.h
@@ -0,0 +1,3 @@
+//configuration of my first_try project
+#define first_try_VERSION_MAJOR @first_try_VERSION_MAJOR@
+#define first_try_VERSION_MINOR @first_try_VERSION_MINOR@
